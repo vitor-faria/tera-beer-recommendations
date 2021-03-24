1e8b9a795861d02d06015802f06ac6de0a8f0c9e
vitorfaria <vitorfaria95@gmail.com>
2021-03-24 14:01:39 -0300

    refactor email message

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from data.build_dataset import preference_map
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+# from email.mime.image import MIMEImage
 from model.create_recommender import get_beer_columns, melt_user_item_matrix
-import streamlit as st
+from os import environ as env
 import pandas as pd
+import smtplib
+import streamlit as st
 from streamlit.hashing import _CodeHasher
 from streamlit.report_thread import get_report_ctx
 from streamlit.server.server import Server
-from turicreate import load_model, SFrame
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-import smtplib
-import pyscreenshot
 from time import sleep
-from os import environ as env
+from turicreate import load_model, SFrame
 
 
 pd.options.mode.chained_assignment = None
@@ -26,7 +25,8 @@ def main():
         "Sugestões": display_sugestoes,
     }
 
-    st.sidebar.title("TeraBeer :beer:")
+    st.sidebar.title("TeraBeer Recommendations :beer:")
+    st.sidebar.text("Recomendações de cerveja artesanal baseadas no seu paladar e uso de IA.")
     page = st.sidebar.selectbox("Selecione uma opção", tuple(pages.keys()))
 
     # Display the selected page with the session state
@@ -45,7 +45,7 @@ def display_pesquisa(state):
     )
   
     st.title(':pencil: Pesquisa')
-    feat_paladar = {}
+    st.text('Responda as perguntas a seguir a respeito do seu paladar e preferências de cerveja.')
 
     options = ['', 'Gosto', 'Não gosto', 'Indiferente', 'Desconheço']
 
@@ -62,7 +62,7 @@ def display_pesquisa(state):
         'Alimento Salgadinho': 'Ruffles/salgadinho',
         'Alimento Tomate': 'Tomate/ketchup',
         'Alimento Margherita': 'Margherita',
-        'Alimento Limonada': 'Limonada/caipirinha',
+        'Alimento Limonada': 'Limonada',
         'Alimento Laranja': 'Suco de laranja',
         'Alimento Maracujá': 'Suco de maracujá',
         'Alimento Tangerina': 'Mexerica/tangerina',
@@ -75,6 +75,7 @@ def display_pesquisa(state):
         'Alimento Café': 'Café sem açúcar'
     }
 
+    feat_paladar = {}
     for feature_name, question in taste_questions.items():
         feat_paladar[feature_name] = st.radio(question, options, index=1)
 
@@ -164,6 +165,7 @@ def display_sugestoes(state):
     df_style_2 = recommended_labels[recommended_labels['rank'] == 2]
     df_style_3 = recommended_labels[recommended_labels['rank'] == 3]
 
+    markdown_list = []
     for df_style in [df_style_1, df_style_2, df_style_3]:
         if not df_style.empty:
             df_style.reset_index(drop=True, inplace=True)
@@ -172,9 +174,16 @@ def display_sugestoes(state):
             style_score = df_style['score'][0]
             style_description = df_style['descricao'][0]
 
-            subheader_text = f'Estilo {style_rank}: {style_name} ({style_score:.1%})'
-            st.subheader(subheader_text)
-            st.markdown(style_description)
+            style_markdown = f"""
+            <div>
+                <h2> Estilo {style_rank}: {style_name} ({style_score:.1%} recomendado para você)</h2>
+                <p>
+                    {style_description}
+                </p>
+            </div>
+            """
+            st.markdown(style_markdown, unsafe_allow_html=True)
+            markdown_list.append(style_markdown)
 
             for index, row in df_style.iterrows():
                 beer = row['cerveja']
@@ -185,32 +194,58 @@ def display_sugestoes(state):
                 count_ratings = int(row['ratings'])
                 figure = row['figura ']
 
-                column1, column2 = st.beta_columns((1, 5))
+                column1, column2 = st.beta_columns((1, 4))
 
                 with column1:
                     try:
-                        st.image(f'fig/{figure}', use_column_width=True)
+                        st.image(
+                            f'fig/{figure}',
+                            # width=200,
+                            use_column_width=True
+                        )
+                        markdown_list.append(f"""
+                        <div>
+                            <img src="/fig/{figure}" alt="Rótulo" style="width:200px;">
+                        </div>
+                        """)
+
                     except FileNotFoundError:
                         st.image('fig/placeholder-image.jpg', use_column_width=True)
+                        markdown_list.append("""
+                        <div>
+                            <img src="/fig/placeholder-image.jpg" alt="Rótulo" style="width:200px;">
+                        </div>
+                        """)
 
                 with column2:
-                    st.markdown(
-                        f"""
-                        ### {beer} - {brewery}
-                        
-                        Nota média: {avg_rating:.3} ({count_ratings} avaliações)
-                        
-                        ABV: {abv}
-                        
-                        IBU: {ibu}
+                    beer_markdown = f"""
+                    <div>
+                        <h3>{beer} - {brewery}</h3>
+                        <p>
+                            <b>Nota média</b>: {avg_rating:.3} ({count_ratings} avaliações) <br>
+                            <b>ABV</b>: {abv}% álcool <br>
+                            <b>IBU</b>: {ibu} unidades de amargor
+                        </p>
+                    </div>
+                    """
+                    st.markdown(beer_markdown, unsafe_allow_html=True)
+                    markdown_list.append(beer_markdown)
 
-                        """
-                    )
+    email = st.text_input('Para receber as sugestões, deixe seu email e aperte Enter:')
+    if email:
+        accept_beer_offers = st.checkbox(
+            'Aceito receber por e-mail ofertas especiais de cervejas com base nas minhas respostas',
+            True
+        )
+        accept_data_usage = st.checkbox(
+            'Permito que utilizem minhas respostas para melhorar recomendações futuras',
+            True
+        )
 
-    email = st.text_input('Para receber sugestoões entre com seu email:')
-    if st.button('Enviar por email'):
-        screenshot()
-        enviar_email(email)
+        if st.button('Enviar recomendações por email'):
+            #  TODO: send all collected data to database
+            enviar_email(email, markdown_list)
+            st.success('Pronto! Confira no seu inbox e, se não encontrar, dá uma olhada na caixa de spam.')
 
 
 class _SessionState:
@@ -289,32 +324,33 @@ def _get_state(hash_funcs=None):
     return session._custom_session_state
 
 
-def screenshot():
-    sugestao = pyscreenshot.grab(bbox=(390, 140, 1850, 890))
-    sugestao.save('D:/Downloads/screenshot/sugestao.png')
-
-
-def enviar_email(email):
+def enviar_email(to_email, markdown_list):
     # Define the source and target email address.
     str_from = env["EMAIL_FROM"]
     sender_pass = env["EMAIL_PASSWORD"]
-    str_to = email
+    str_to = to_email
 
     # Create an instance of MIMEMultipart object, pass 'related' as the constructor parameter.
     message = MIMEMultipart('related')
-    message['Subject'] = 'Recomendações TeraBeer'
+    message['Subject'] = 'Sua recomendação TeraBeer'
     message['From'] = str_from
     message['To'] = str_to
 
-    msg_text = MIMEText('<b>Esta é a sua recomendação:<br><img src="cid:image1"><br>', 'html')
+    content = ' '.join(markdown_list)
+    # html_content = markdown.markdown(content)
+    message_html = f"""
+    <b>Esta é a sua recomendação</b>:<br>
+    <div> {content} </div>
+    """
+    msg_text = MIMEText(message_html, 'html')
     # Attach the above html content MIMEText object to the menssage object.
     message.attach(msg_text)
-    fp = open('D:/Downloads/screenshot/sugestao.png', 'rb')
-    msg_image = MIMEImage(fp.read())
-    fp.close()
+    # fp = open('D:/Downloads/screenshot/sugestao.png', 'rb')
+    # msg_image = MIMEImage(fp.read())
+    # fp.close()
 
-    msg_image.add_header('Content-ID', '<image1>')
-    message.attach(msg_image)
+    # msg_image.add_header('Content-ID', '<image1>')
+    # message.attach(msg_image)
 
     # Create an smtplib.SMTP object to send the email.
     smtp = smtplib.SMTP('smtp.gmail.com', 587)
